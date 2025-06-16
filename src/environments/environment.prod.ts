d63714144c5293e3a472bd1f59817ac429cd9146
Rama <yerram.reddy@satisfic.com>
2025-06-16 16:31:26 +0530

    Removing SSR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://quickchat-backend-tc99.onrender.com/api',
-  socketUrl:
-    'https://quickchat-backend-tc99.onrender.com',
+  apiUrl: 'https://quickchat-backend-qlxr.onrender.com/api',
+  socketUrl: 'https://quickchat-backend-qlxr.onrender.com',
 };
